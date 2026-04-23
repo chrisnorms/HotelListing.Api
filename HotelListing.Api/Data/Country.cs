9017d77ca3475a07561da6299345120f3a878611
Norman <Norman@DESKTOP-BG76A5G>
2026-04-23 15:09:37 +0800

    Added Hotel and Country entity models
    - Established a one is to many relationship between Country and Hotel
    - Setup the database configuration like the connectionstring using EF Core
    - Setup the EF Core DBContext and DBSet

---
+++ HotelListing.Api/Data/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Api.Data
+{
+    public class Country
+    {
+        [Key]
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public IList<Hotel> Hotels { get; set; } = [];
+    }
+}
